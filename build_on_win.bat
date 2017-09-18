ae6314ff45263a80809ed6cc93766c8c119eeb1e
Salvador Pardiñas <darkfm@vera.com.uy>
2017-09-18 01:01:52 -0300

    Oh my god

--- build_on_win.bat
+++ build_on_win.bat
@@ -1,2 +1,3 @@
 set fltkinc=C:\Users\user\Desktop\fltk\fltk-1.3.4-1
+%fltkinc%\build\bin\Release\fluid.exe -c app.fl
 cl main.cpp %fltkinc%\build\lib\Release\*.lib *.obj /I %fltkinc% advapi32.lib comdlg32.lib ole32.lib shell32.lib wsock32.lib user32.lib gdi32.lib /MD /Fe Honkinator.exe
