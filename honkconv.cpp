794032fcc09bf3bf455b68b3bdc555461f3d75ad
Salvador Pardiñas <darkfm@vera.com.uy>
2017-09-17 21:32:34 -0300

    Swapped to timidity

--- honkconv.cpp
+++ honkconv.cpp
@@ -3,16 +3,14 @@
 #include <cstdlib>
 void honk(const char* midloc, const char* wavloc)
 {
-#ifdef _WIN32
-	char* cmd = (char*)malloc(strlen("fluidsynth.exe honk.sf2 -F ")
-#else
-	char* cmd = (char*)malloc(strlen("fluidsynth honk.sf2 -F ")
-#endif
-				 +strlen(wavloc)+strlen(midloc)+8);
-	strcpy(cmd,"fluidsynth honk.sf2 -F ");
+	char* cmd = (char*)malloc(strlen("timidity -c tim.cfg -OF -o")
+				 +strlen(wavloc)+strlen(midloc)+12);
+	strcpy(cmd,"timidity -c tim.cfg -OF -o\"");
 	strcat(cmd,wavloc);
-	strcat(cmd," ");
+	strcat(cmd,"\" \"");
 	strcat(cmd,midloc);
+	strcat(cmd,"\"");
 	system(cmd);
+	free(cmd);
 	return;
 }
